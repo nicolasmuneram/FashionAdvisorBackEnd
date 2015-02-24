4341954a09fa5fd3395ea1e7b2a42821e711c809
Nicolas Munera <nmunera@eafit.edu.co>
2015-02-23 23:07:31 -0500

    errors

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,25 @@
+require 'shopsense'	
+class ProductsController < ApplicationController
+
+	def index
+		@products = []
+
+        if params[:id]
+            client = Shopsense::API.new({'partner_id' => 'uid9921-26902161-26'})
+            response = client.search(params[:id])
+            raw_products = JSON.parse(response)["products"]
+            puts raw_products.inspect
+
+            @products = raw_products.map! do |product|
+                image = product["images"].select { |i| i["sizeName"] == 'Large' }.pop
+                puts image.inspect
+                {
+                  'name' => product["name"],
+                  'image' => image
+                }
+            end
+            @search = params[:id]
+        end
+        @products
+	end
+end
