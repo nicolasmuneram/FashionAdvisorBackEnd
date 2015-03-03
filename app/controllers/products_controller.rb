2c9ddeced3a3770395e919eb85510f97104b5769
Daniel Lopera <dlopera3@eafit.edu.co>
2015-03-02 23:32:16 -0500

    Changes to product view and product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,28 @@
 require 'shopsense'	
+
 class ProductsController < ApplicationController
     before_action :authenticate_user!
 
-	def index
+	def search
 		@products = []
 
         if params[:id]
-            client = Shopsense::API.new({'partner_id' => 'uid9921-26902161-26'})
-            response = client.search(params[:id])
-            raw_products = JSON.parse(response)["products"]
-            puts raw_products.inspect
 
+            client = Shopsense::API.new({'partner_id' => 'uid9921-26902161-26'})
+            response = client.search(params[:id], 0, params[:num_results]||=10)
+            raw_products = JSON.parse(response)["products"]
             @products = raw_products.map! do |product|
                 image = product["images"].select { |i| i["sizeName"] == 'Large' }.pop
-                puts image.inspect
                 {
                   'name' => product["name"],
-                  'image' => image
+                  'image' => image,
+                  'description' => product["description"],
+                  'price' => product["price"],
+                  'url' => product["url"]
                 }
             end
             @search = params[:id]
-        end
-        @products
+        end 
+        render :json => {:products => @products, :auth_token => current_user.authentication_token }.to_json, :status => :ok
 	end
 end
