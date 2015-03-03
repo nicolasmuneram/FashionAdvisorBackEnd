88ebfdc752bebaac63760d59f5f7aef26b110098
Nicolas Munera <nmunera@eafit.edu.co>
2015-03-03 16:35:52 -0500

    Standarized the way the sessions controller returns its json.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,12 +5,12 @@ class Users::SessionsController < Devise::SessionsController
     resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
-    render json: {response: 'Login succesful', auth_token: current_user.authentication_token}
+    render json: {response: 'Login succesful', auth_token: current_user.authentication_token, email: resource.email}
   end
 
   # When called, it means that the login has failed.
   def failure
-     render json: {response: false, auth_token: nil}
+     render json: {response: false, auth_token: nil, email: nil}
   end
 
   
