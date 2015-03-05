fdd122b5a1bbd95bdd47d63bcaa19a96486f6046
Daniel Klinkert <daniel.klinkert@gmail.com>
2015-03-05 17:20:00 -0500

    fix bug with authenticity token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  skip_before_filter :verify_authenticity_token
   protect_from_forgery with: :null_session
   #From simple token authentication gem.
   acts_as_token_authentication_handler_for User
 
 
 end
-
