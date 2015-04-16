b23e9d489cac831795303bc0dac21a049e148601
Daniel Klinkert <daniel.klinkert@gmail.com>
2015-04-16 16:20:19 -0500

    add user model and other necessary files

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
 
-
   has_and_belongs_to_many :products
 
-  # @param [Integer] product_id, this is the ID of the product we are looking for in the wardrobe
-  # @return [Boolean] True if the product is in the users wardrobe.
+  #@param [Integer] product_id, this is the ID of the product we are looking for in the wardrobe
+  #@return [Boolean] True if the product is in the users wardrobe.
   def has_product_in_wardrobe? (product_id)
       products.any? { |element| element.id == product_id}
   end
-
 end
